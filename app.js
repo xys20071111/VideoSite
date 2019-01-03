cf1022c9ad9fe8f77d55f09dba9bc5810f01e39a
xys20071111 <xys20071111@163.com>
2019-01-03 18:00:01 +0800

    大体造了个页面

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const path = require('path');
 const ejs = require('ejs');
+const fs = require('fs');
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static('video'));
-app.use(express.static('static'))
+app.use(express.static('static'));
+app.use('/player',(req,res)=>{
+  let video = {src:req.query.id + ".mp4",name:"TEST"};
+  res.set('Content-Type','text/html');
+  res.send(ejs.render(fs.readFileSync("./views/player.ejs"),{video:video}));
+});
+app.listen(80);
