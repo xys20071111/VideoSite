6ba6449d5fecbc48da05a6479caa79757075562a
xys20071111 <xys20071111@163.com>
2019-01-03 23:25:33 +0800

    Fix bug

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
+const mysql = require('mysql');
 const app = express();
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static('video'));
-app.use(express.static('static'));
+app.use('/video',express.static('video'));
+app.use('/static',express.static('static'));
 app.use('/player',(req,res)=>{
-  let video = {src:req.query.id + ".mp4",name:"TEST"};
   res.set('Content-Type','text/html');
-  res.send(ejs.render(fs.readFileSync("./views/player.ejs",'utf-8')));
+  res.send(ejs.render(fs.readFileSync("./views/player.ejs",'utf-8'),{video:video}));
 });
 app.listen(80);
