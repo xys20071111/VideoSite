2407efff5f1647ad93168c5cc53d76cd6da3cf76
xys20071111 <xys20071111@163.com>
2019-01-04 17:48:21 +0800

    完善一下信息查询

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ db.connect(function(err){if(err){throw err;}});
 app.use('/video',express.static('video'));
 app.use('/static',express.static('static'));
 app.use('/player',(req,res)=>{
+  let video = {};
+    db.query('select * from videoList where id=?',[req.query.id],(err,result)=>{
+      if(err){
+        throw err;
+      }
+      video.src = '/video/' + result.id + '.mp4';
+      video.name = result.name;
+    });
   res.set('Content-Type','text/html');
   res.send(ejs.render(fs.readFileSync("./views/player.ejs",'utf-8'),{video:video}));
 });
