7a0bac8bf2d7c0134738c73649b054b57af3a3e2
xys20071111 <xys20071111@163.com>
2019-01-05 16:45:14 +0800

    学校的32位机，装不了MySQL,无法测试

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const app = express();
 db.connect(function(err){if(err){throw err;}});
 app.use('/video',express.static('video'));
 app.use('/static',express.static('static'));
+//播放页
 app.use('/player',(req,res)=>{
   let video = {};
-    db.query('select * from videoList where id=?',[req.query.id],(err,result)=>{
+    db.query('select * from videoList where id=' + db.escape(req.query.id) + ';',(err,result)=>{
       if(err){throw err;}
       video.src = '/video/' + result[0].id + '.mp4';
       video.name = result[0].name;
@@ -18,10 +19,11 @@ app.use('/player',(req,res)=>{
   res.set('Content-Type','text/html');
   res.send(ejs.render(fs.readFileSync("./views/player.ejs",'utf-8'),{video:video}));
 });
+//首页
 app.use('/index',index);
 app.use('/',index);
 function index(req,res){
-  let info;
+  let info = null;
   db.query('select * from videoList',(err,result)=>{
     if(err){throw err;}
     info=result;
