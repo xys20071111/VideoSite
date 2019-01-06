fa8d1d92195a0d541aa2d7147e9edbe299fd4adb
xys20071111 <xys20071111@163.com>
2019-01-06 18:18:07 +0800

    删几行没用的代码

--- app.js
+++ app.js
@@ -12,28 +12,25 @@ db.connect(function(err){if(err){throw err;}});
 app.use('/video',express.static('video'));
 app.use('/static',express.static('static'));
 //播放页
-app.use('/player',(req,res)=>{
-    db.query('select * from videoList where id=' + db.escape(req.query.id) + ';',(err,result)=>{
-      if(err){throw err;}
-      let video = {};
-      video.src = '/video/' + result[0].id + '.mp4';
-      video.name = result[0].name;
-      res.set('Content-Type','text/html');
-      res.send(ejs.render(fs.readFileSync("./views/player.ejs",'utf-8'),{video:video}));
-      console.log(video)
-    });
-});
+app.use('/player',player);
 //首页
-
 app.use('/',index);
+app.listen(80);
 function index(req,res){
-  let info = null;
   db.query('select * from videoList',(err,result)=>{
     if(err){throw err;}
-    info=result;
     res.set('Content-Type','text/html');
     res.send(ejs.render(fs.readFileSync("./views/index.ejs",'utf-8'),{index:result}));
   });
-
 }
-app.listen(80);
+function player(req,res){
+  db.query('select * from videoList where id=' + db.escape(req.query.id) + ';',(err,result)=>{
+    if(err){throw err;}
+    let video = {};
+    video.src = '/video/' + result[0].id + '.mp4';
+    video.name = result[0].name;
+    res.set('Content-Type','text/html');
+    res.send(ejs.render(fs.readFileSync("./views/player.ejs",'utf-8'),{video:video}));
+    console.log(video)
+  });
+}
