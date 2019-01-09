0f58b6524229855e9d09c31644618b7d320be741
xys20071111 <xys20071111@163.com>
2019-01-09 18:13:21 +0800

    加注释

--- app.js
+++ app.js
@@ -8,13 +8,45 @@ const config = require('./config');
 const db = mysql.createConnection(config.db);
 const app = express();
 db.connect(function(err){if(err){throw err;}});
-
+//静态资源
 app.use('/video',express.static('video'));
 app.use('/static',express.static('static'));
 //播放页
 app.use('/player',player);
 //介绍页
-app.use('/intro',(req,res)=>{
+app.use('/intro',intro);
+//首页
+app.use('/',index);
+//监听端口号
+app.listen(config.port);
+//输出端口号
+if(config.port == 80)
+  console.log("Server is running on http://127.0.0.1");
+else {
+  console.log("Server is running on http://127.0.0.1:%d",config.port);
+}
+//渲染首页
+function index(req,res){
+  db.query('select * from videoList',(err,result)=>{
+    if(err){throw err;}
+    res.set('Content-Type','text/html');
+    res.send(ejs.render(fs.readFileSync("./views/index.ejs",'utf-8'),{index:result}));
+  });
+}
+//渲染播放页
+function player(req,res){
+  db.query('select * from videoList where id=' + db.escape(req.query.id) + ';',(err,result)=>{
+    if(err){throw err;}
+    let video = {};
+    video.src = '/video/' + result[0].filename;
+    video.name = result[0].name;
+    res.set('Content-Type','text/html');
+    res.send(ejs.render(fs.readFileSync("./views/player.ejs",'utf-8'),{video:video}));
+    //console.log(video)
+  });
+}
+//渲染介绍页
+function intro(req,res){
   db.query('select * from videoList where id=' + db.escape(req.query.id) + ';',(err,result)=>{
     if(err){throw err;}
     let video = {};
@@ -25,32 +57,6 @@ app.use('/intro',(req,res)=>{
     //console.log(result[0])
     res.set('Content-Type','text/html');
     res.send(ejs.render(fs.readFileSync("./views/intro.ejs",'utf-8'),{video:video}));
-    //console.log(video)
-  });
-});
-//首页
-app.use('/',index);
-app.listen(config.port);
-if(config.port == 80)
-  console.log("Server is running on http://127.0.0.1");
-else {
-  console.log("Server is running on http://127.0.0.1:%d",config.port);
-}
-function index(req,res){
-  db.query('select * from videoList',(err,result)=>{
-    if(err){throw err;}
-    res.set('Content-Type','text/html');
-    res.send(ejs.render(fs.readFileSync("./views/index.ejs",'utf-8'),{index:result}));
-  });
-}
-function player(req,res){
-  db.query('select * from videoList where id=' + db.escape(req.query.id) + ';',(err,result)=>{
-    if(err){throw err;}
-    let video = {};
-    video.src = '/video/' + result[0].filename;
-    video.name = result[0].name;
-    res.set('Content-Type','text/html');
-    res.send(ejs.render(fs.readFileSync("./views/player.ejs",'utf-8'),{video:video}));
     //console.log(video)
   });
 }
